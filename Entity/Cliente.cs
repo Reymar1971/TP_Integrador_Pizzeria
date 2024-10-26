2c0baea9038fa71b5c806d51c30203175e5c09b5
AndresLuisG <andres_sanluis@yahoo.com.ar>
2024-10-26 13:18:43 -0300

    Se crean los campos de las clases

--- Entity/Cliente.cs
+++ Entity/Cliente.cs
@@ -8,7 +8,14 @@ namespace Entity
 {
     public class Cliente
     {
+        private int idCliente;
+        private string nombreApellido;
+        private string direccion;
+        private string telefono;
 
-
+        public int IdCliente { get => idCliente; set => idCliente = value; }
+        public string NombreApellido { get => nombreApellido; set => nombreApellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
     }
 }
--- Entity/Pedido.cs
+++ Entity/Pedido.cs
@@ -8,6 +8,20 @@ namespace Entity
 {
     public class Pedido
     {
+        private int idPedido;
+        private int idCliente;
+        private int idproducto;
+        private int cantidad;
+        private string codigoComprobante;
+        private DateTime fecha;
+        private decimal total;
 
+        public int IdPedido { get => idPedido; set => idPedido = value; }
+        public int IdCliente { get => idCliente; set => idCliente = value; }
+        public int Idproducto { get => idproducto; set => idproducto = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+        public string CodigoComprobante { get => codigoComprobante; set => codigoComprobante = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public decimal Total { get => total; set => total = value; }
     }
 }
