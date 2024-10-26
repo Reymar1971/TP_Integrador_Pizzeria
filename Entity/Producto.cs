cecc362236e6dfc6704c12a0f1dae79d3415e48a
AndresLuisG <andres_sanluis@yahoo.com.ar>
2024-10-26 12:21:16 -0300

    Cracion de la clase Producto

---
+++ Entity/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Producto
+    {
+        // ests es la clase Pruducto del proyecto Pizzeria
+    }
+}
