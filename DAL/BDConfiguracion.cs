c292052e9d52b183589771eb1d709940d20e9a9e
AndresLuisG <andres_sanluis@yahoo.com.ar>
2024-10-26 11:37:25 -0300

    Agregar archivos de proyecto.

---
+++ DAL/BDConfiguracion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    static class BDConfiguracion
+    {
+        private const string BD_CONFIG_NAME = "BDConexion";
+
+        public static string getConectionBD()
+        {
+            return ConfigurationManager.ConnectionStrings[BD_CONFIG_NAME].ConnectionString;
+        }
+
+    }
+}
---
+++ UI/FrmPrincipal.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace UI
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["BDConexion"].ConnectionString);
+
+            using (connection)
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Conexión exitosa!");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
