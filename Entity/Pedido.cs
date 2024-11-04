2c9f6070cf21aa7c6ba37242da6f4ffe945fd23b
AndresLuisG <andres_sanluis@yahoo.com.ar>
2024-11-04 14:58:03 -0300

    Se corrige nomenglatura en propiedades de clase Pedidos

--- Entity/Pedido.cs
+++ Entity/Pedido.cs
@@ -13,15 +13,15 @@ namespace Entity
         private string codigoComprobante;
         private DateTime fecha;
         private decimal total;
-        private Cliente Cliente;
-        private Producto Producto;
+        private Cliente cliente;
+        private Producto producto;
 
         public int IdPedido { get => idPedido; set => idPedido = value; }
         public int Cantidad { get => cantidad; set => cantidad = value; }
         public string CodigoComprobante { get => codigoComprobante; set => codigoComprobante = value; }
         public DateTime Fecha { get => fecha; set => fecha = value; }
         public decimal Total { get => total; set => total = value; }
-        public Cliente Cliente1 { get => Cliente; set => Cliente = value; }
-        public Producto Producto1 { get => Producto; set => Producto = value; }
+        public Cliente Cliente { get => cliente; set => cliente = value; }
+        public Producto Producto { get => producto; set => producto = value; }
     }
 }
